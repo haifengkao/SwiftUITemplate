e6c551d09356fbcfb1836272d250f6089384e7ae
Hai Feng Kao <haifeng@cocoaspice.in>
2022-11-25 18:42:18 +0800

    switch to official version of nimble and quick

--- ProjectDescriptionHelpers/SwiftPackage+Default.swift
+++ ProjectDescriptionHelpers/SwiftPackage+Default.swift
@@ -9,10 +9,10 @@ import Foundation
 
 public extension Module {
     static var Quick: Module {
-        .package(.init(name: "Quick", url: "https://github.com/Quick/Quick", requirement: .branch("main"), unitTestTool: true))
+        .package(.init(name: "Quick", url: "https://github.com/Quick/Quick", requirement: .upToNextMajor(from: "6.0.1"), unitTestTool: true))
     }
 
     static var Nimble: Module {
-        .package(.init(name: "Nimble", url: "https://github.com/Quick/Nimble", requirement: .branch("main"), unitTestTool: true))
+        .package(.init(name: "Nimble", url: "https://github.com/Quick/Nimble", requirement: .upToNextMajor(from: "11.2.1"), unitTestTool: true))
     }
 }
