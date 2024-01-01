39b4a465111fed6bd8aaa67146b2048e05df9fa0
Hai Feng Kao <haifeng@cocoaspice.in>
2024-01-01 17:43:10 +0800

    remove unused file

--- fixtures/Example1/tt.swift
+++
@@ -1,8 +0,0 @@
-let package = Package(
-    name: "PackageName",
-    dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "13.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "7.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.5.0")
-    ]
-)
