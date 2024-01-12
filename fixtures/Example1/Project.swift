61f56d6ec67671c1503d73649f5255d15e25d6a8
Hai Feng Kao <haifeng@cocoaspice.in>
2024-01-12 14:30:10 +0800

    remove unused config

--- fixtures/Example1/Project.swift
+++ fixtures/Example1/Project.swift
@@ -3,8 +3,6 @@ import ProjectDescriptionHelpers
 import SwiftUITemplate
 
 let project: Project = {
-    GenerationConfig.default.destinations = destinations
-
     return Project(name: "App",
                    organizationName: "example.SwiftUITemplate",
 
